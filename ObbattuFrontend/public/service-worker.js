7919772d30aa255963c11b49af3af34ef694aed9
skandabhairava <skandabhairava@gmail.com>
2022-07-30 20:03:54 +0530

    debug

--- ObbattuFrontend/public/service-worker.js
+++ ObbattuFrontend/public/service-worker.js
@@ -2,7 +2,7 @@
 // Cache Name
 const CACHE_NAME = "static-cache-v1";
 // Cache Files
-const FILES_TO_CACHE = ["/index.html"];
+const FILES_TO_CACHE = ["/", "/global.css", "/stats.png", "/build/bundle.css", "/build/bundle.js"];
 // install
 self.addEventListener("install", (evt) => {
   console.log("[ServiceWorker] Install");
@@ -17,6 +17,19 @@ self.addEventListener("install", (evt) => {
 // Active PWA Cache and clear out anything older
 self.addEventListener("activate", (evt) => {
   console.log("[ServiceWorker] Activate");
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 // listen for fetch events in page navigation and return anything that has been cached
 self.addEventListener("fetch", (evt) => {
@@ -28,7 +41,7 @@ self.addEventListener("fetch", (evt) => {
   evt.respondWith(
     fetch(evt.request).catch(() => {
       return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match("index.html");
+        return cache.match("/");
       });
     })
   );
