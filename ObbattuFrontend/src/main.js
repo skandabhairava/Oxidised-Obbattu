0c27652e6579f77a3e9fa605c8213b5ed3013cee
skandabhairava <skandabhairava@gmail.com>
2022-07-30 19:36:59 +0530

    debug

--- ObbattuFrontend/src/main.js
+++ ObbattuFrontend/src/main.js
@@ -6,9 +6,10 @@ const app = new App({
 	}
 });
 
-console.log(navigator)
 if ("serviceWorker" in navigator) {
 	navigator.serviceWorker.register("./service-worker.js")
+	.then((data) => console.log("Service worker registered", data))
+	.catch((err) => console.log("err occurred while loading service worker", err))
 }
 
 export default app;
